94b1c8a63121f82ff238a127acd7bc517b1ea7a5
barrasso <mark.barrasso@live.com>
2014-10-10 21:49:11 -0400

    correctly displaying chess board

--- Lab4_EightQueensPuzzle/Form1.cs
+++ Lab4_EightQueensPuzzle/Form1.cs
@@ -23,20 +23,32 @@ namespace Lab4_EightQueensPuzzle
             this.BackColor = Color.Gray;
         }
 
-        // Set the board size (height and width)
-        int boardsize = 400;
-
-        // Set board coordinates (x and y)
-        int boardXPosition, boardYPosition = 100;
-
         // Init brushes for cell color array
         Brush[] cellColor = {Brushes.Black, Brushes.White};
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
+            // Set the board size (height and width)
+            int boardSize = 400;
+
+            // Set board coordinates (x and y)
+            int boardPosition = 100;
+
+            // Set size of cells
+            int cellSize = boardSize / 8;
+
             // Get graphics object
             Graphics g = e.Graphics;
 
+            // Nested loop to create chess board
+            for (int col = 0; col < 8; col++)
+            {
+                for (int row = 0; row < 8; row ++)
+                {
+                    // Fill rectangles with alernating colors, with each cell 1/8th of the board's size
+                    g.FillRectangle(cellColor[(col + row) % 2], boardPosition + col * cellSize, boardPosition + row * cellSize, cellSize, cellSize);
+                }
+            }
         }
     }
 }
