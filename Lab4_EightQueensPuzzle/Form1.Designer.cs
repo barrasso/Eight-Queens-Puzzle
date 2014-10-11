5b08b34971a9f770eb2fda0a1c755e61a83f1939
barrasso <mark.barrasso@live.com>
2014-10-11 15:11:09 -0400

    created Queen class and added form features, reworked code

--- Lab4_EightQueensPuzzle/Form1.Designer.cs
+++ Lab4_EightQueensPuzzle/Form1.Designer.cs
@@ -28,21 +28,50 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(199, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(174, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "You have {0} queens on the board.";
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Gray;
+            this.button1.Location = new System.Drawing.Point(96, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
             this.ClientSize = new System.Drawing.Size(609, 605);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Lab4_EightQueensPuzzle/Form1.cs
+++ Lab4_EightQueensPuzzle/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,6 +13,24 @@ namespace Lab4_EightQueensPuzzle
 {
     public partial class Form1 : Form
     {
+        // Create array list to hold cells
+        public ArrayList cellArray = new ArrayList();
+
+        // Create array list to hold queens
+        public ArrayList queenArray = new ArrayList();
+
+        // Init brushes for cell color array
+        Brush[] cellColor = {Brushes.White, Brushes.Black};
+
+        // Set border color and size
+        Pen blackPen = new Pen(Color.FromArgb(255, 0, 0, 0), 2);
+
+        // Set the board size (height and width)
+        int boardSize = 400;
+
+        // Set board coordinates (x and y)
+        int boardPosition = 100;
+
         public Form1()
         {
             InitializeComponent();
@@ -19,43 +38,42 @@ namespace Lab4_EightQueensPuzzle
             // Set form title text
             this.Text = @"Eight Queens by Mark Barrasso";
 
-            // Set background color of form
-            this.BackColor = Color.Gray;
+            // Set queen number label text
+            label1.Text = String.Format("You have {0} queens on the board", queenArray.Count);
         }
-
-        // Init brushes for cell color array
-        Brush[] cellColor = {Brushes.White, Brushes.Black};
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
-            // Set the board size (height and width)
-            int boardSize = 400;
-
-            // Set board coordinates (x and y)
-            int boardPosition = 100;
+            // Get graphics object
+            Graphics g = e.Graphics;
 
             // Set size of cells
             int cellSize = boardSize / 8;
 
-            // Set border color and size
-            Pen blackPen = new Pen(Color.FromArgb(255, 0, 0, 0), 2);
-
-            // Get graphics object
-            Graphics g = e.Graphics;
-
             // Nested loop to create chess board
             for (int col = 0; col < 8; col++)
             {
-                for (int row = 0; row < 8; row ++)
+                for (int row = 0; row < 8; row++)
                 {
                     // Make rectangle with black border
                     Rectangle rect = new Rectangle(boardPosition + col * cellSize, boardPosition + row * cellSize, cellSize, cellSize);
+
+                    // Make rectangle with black border
                     g.DrawRectangle(blackPen, rect);
 
                     // Fill rectangles with alernating colors (white and black)
-                    g.FillRectangle(cellColor[(col + row) % 2], rect);
+                    g.FillRectangle(cellColor[(row + col) % 2], rect);
                 }
             }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Clear the queens
+            this.queenArray.Clear();
+
+            // Invalidate
+            this.Invalidate();
         }
     }
 }
