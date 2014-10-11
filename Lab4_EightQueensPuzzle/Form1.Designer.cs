ffb873ba0024262df2e46067a7b138cb7eaf875d
barrasso <mark.barrasso@live.com>
2014-10-10 21:26:43 -0400

    init variables and paint event

--- Lab4_EightQueensPuzzle/Form1.Designer.cs
+++ Lab4_EightQueensPuzzle/Form1.Designer.cs
@@ -37,6 +37,7 @@
             this.ClientSize = new System.Drawing.Size(852, 566);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
             this.ResumeLayout(false);
 
         }
--- Lab4_EightQueensPuzzle/Form1.cs
+++ Lab4_EightQueensPuzzle/Form1.cs
@@ -15,6 +15,28 @@ namespace Lab4_EightQueensPuzzle
         public Form1()
         {
             InitializeComponent();
+
+            // Set form title text
+            this.Text = @"Eight Queens by Mark Barrasso";
+
+            // Set background color of form
+            this.BackColor = Color.Gray;
+        }
+
+        // Set the board size (height and width)
+        int boardsize = 400;
+
+        // Set board coordinates (x and y)
+        int boardXPosition, boardYPosition = 100;
+
+        // Init brushes for cell color array
+        Brush[] cellColor = {Brushes.Black, Brushes.White};
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            // Get graphics object
+            Graphics g = e.Graphics;
+
         }
     }
 }
