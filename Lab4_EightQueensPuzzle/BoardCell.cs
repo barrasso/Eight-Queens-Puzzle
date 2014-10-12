a230ac6c2dfe7ce0f5c7cebc93a8d88caaea9786
barrasso <mark.barrasso@live.com>
2014-10-11 23:27:42 -0400

    boardcell class implemented

---
+++ Lab4_EightQueensPuzzle/BoardCell.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Threading.Tasks;
+
+namespace Lab4_EightQueensPuzzle
+{
+    public class BoardCell
+    {
+        // Cell row and column
+        public int rowNumber;
+        public int colNumber;
+
+        // Cell origin coords
+        public Point cellOrigin;
+
+        // Cell center coords
+        public Point cellCenter;
+
+        public Brush cellColor;
+
+        // Default constructor
+        public BoardCell(Rectangle cell, Brush color, int row,int column)
+        {
+            // The point in the center of the cell
+            this.cellCenter = new Point(cell.X + 25, cell.Y + 25);
+
+            // Set coords for origin
+            this.cellOrigin = new Point(cell.X, cell.Y);
+
+            // Set cell color
+            this.cellColor = color;
+
+            // Set cell labels
+            this.rowNumber = row;
+            this.colNumber = column;
+        }
+    }
+}
--- Lab4_EightQueensPuzzle/Form1.cs
+++ Lab4_EightQueensPuzzle/Form1.cs
@@ -13,23 +13,32 @@ namespace Lab4_EightQueensPuzzle
 {
     public partial class Form1 : Form
     {
-        // Create array list to hold cells
-        public ArrayList cellArray = new ArrayList();
+        // Holds all the chess board cells 
+        public List<BoardCell> allCells = new List<BoardCell>();
+
+        // Create array list to hold cell info
+        public ArrayList cellInfoArray = new ArrayList();
 
         // Create array list to hold queens
         public ArrayList queenArray = new ArrayList();
 
         // Init brushes for cell color array
-        Brush[] cellColor = {Brushes.White, Brushes.Black};
+        public Brush[] cellColor = {Brushes.White, Brushes.Black};
 
         // Set border color and size
-        Pen blackPen = new Pen(Color.FromArgb(255, 0, 0, 0), 2);
+        public Pen blackPen = new Pen(Color.FromArgb(255, 0, 0, 0), 2);
 
         // Set the board size (height and width)
-        int boardSize = 400;
+        public int boardSize = 400;
 
         // Set board coordinates (x and y)
-        int boardPosition = 100;
+        public int boardPosition = 100;
+
+        // Set size of cells
+        public int cellSize = 50;
+
+        // Safe flag
+        public bool isSafe;
 
         public Form1()
         {
@@ -40,15 +49,28 @@ namespace Lab4_EightQueensPuzzle
 
             // Set queen number label text
             label1.Text = String.Format("You have {0} queens on the board", queenArray.Count);
+
+            // Nested loop to create chess board
+            for (int col = 0; col < 8; col++)
+            {
+                for (int row = 0; row < 8; row++)
+                {
+                    // Cell color
+                    Brush currentColor = cellColor[(row + col) % 2];
+
+                    // Make cell to hold coords
+                    Rectangle cell = new Rectangle(boardPosition + col * cellSize, boardPosition + row * cellSize, cellSize, cellSize);
+
+                    // Instantiate board cell
+                    BoardCell chessBoardCell = new BoardCell(cell, currentColor, row.col);
+                }
+            }
         }
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
             // Get graphics object
             Graphics g = e.Graphics;
-
-            // Set size of cells
-            int cellSize = boardSize / 8;
 
             // Nested loop to create chess board
             for (int col = 0; col < 8; col++)
@@ -58,18 +80,20 @@ namespace Lab4_EightQueensPuzzle
                     // Make rectangle with black border
                     Rectangle rect = new Rectangle(boardPosition + col * cellSize, boardPosition + row * cellSize, cellSize, cellSize);
 
+
+
                     // Make rectangle with black border
                     g.DrawRectangle(blackPen, rect);
 
                     // Fill rectangles with alernating colors (white and black)
-                    g.FillRectangle(cellColor[(row + col) % 2], rect);
+                    g.FillRectangle(brush.cellColor, rect);
                 }
             }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            // Clear the queens
+            // Clear the queens array
             this.queenArray.Clear();
 
             // Invalidate
